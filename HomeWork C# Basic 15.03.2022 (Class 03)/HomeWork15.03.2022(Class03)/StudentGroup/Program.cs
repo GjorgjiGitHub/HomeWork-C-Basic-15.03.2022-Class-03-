bb21f3ba6405db18095d5146e17b76bd9d49f75e
GjorgjiGitHub <98532895+GjorgjiGitHub@users.noreply.github.com>
2022-03-22 14:35:30 +0100

    Add files via upload

---
+++ HomeWork C# Basic 15.03.2022 (Class 03)/HomeWork15.03.2022(Class03)/StudentGroup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////////////
+            string[] studentsG1 = { "Andrijana Saliloska", "Menche Saliloska", "Mara Saliloska", "Ile Saliloski", "Julija Saliloska" };
+            string[] studentsG2 = { "Zlatan Dimitrov", "Zaklina Dimitrova", "Marija Dimitrova", "Gjorgji Dimitrov", "Nikola Oreshkov" };
+
+            Console.WriteLine("Choose between 1 or 2 to print students info...");
+            int userChoice = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                if (userChoice == 1)
+                {
+                    Console.WriteLine((i + 1) + ". " + studentsG1[i]);
+                }
+                else if (userChoice == 2)
+                {
+                    Console.WriteLine((i + 1) + ". " + studentsG2[i]);
+                }
+                else
+                {
+                    Console.WriteLine("You typed wrong number!");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+            /////////////////////////////////////////////////////////////////////////
+        }
+    }
+}
---
+++ HomeWork C# Basic 15.03.2022 (Class 03)/HomeWork15.03.2022(Class03)/SumOfEven/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////////////////////
+            Console.WriteLine("Enter first number:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter fourth number:");
+            int number4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter fifth number:");
+            int number5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter sixth number:");
+            int number6 = int.Parse(Console.ReadLine());
+            int[] arrOfNumbers = { number1, number2, number3, number4, number5, number6 };
+            int sum = 0;
+            for (int i = 0; i < arrOfNumbers.Length; i++)
+            {
+                if (arrOfNumbers[i] % 2 == 0)
+                {
+                    sum += arrOfNumbers[i];
+                }
+            }
+            Console.WriteLine("Sum of evens is " + sum);
+            Console.ReadLine();
+            //////////////////////////////////////////////////////////////////
+        }
+    }
+}
